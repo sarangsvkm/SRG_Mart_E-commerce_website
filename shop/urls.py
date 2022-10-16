9fe02c16607c9e162129886bfd1f2d022b3da953
SARANG S <sarangsvkm@gmail.com>
2022-10-16 21:08:14 +0530

    first

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ app_name='shop'
 urlpatterns = [
     path('', views.allProdCat,name='allProdCat'),
     path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
-path('<slug:c_slug>/<slug:product_slug>/',views.allProdCat,name='prodCatdetail')
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatdetail')
 
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Category,Product
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
 
 # Create your views here.
 # def index(request):
 #     return HttpResponse("haii, welcome")
 
-def allProdCat(request,c_slug=None,):
+def allProdCat(request,c_slug=None):
     c_page=None
-    products=None
+    products_list=None
     if c_slug!=None:
         c_page=get_object_or_404(Category,slug=c_slug)
-        products=Product.objects.all().filter(Category=c_page,available=True)
+        products_list=Product.objects.all().filter(Category=c_page,available=True)
     else:
-        products=Product.objects.all().filter(available=True)
-    return render(request,"category.html",{'category':c_page,'products':products})
-def  proDetail(request,c_slug,Product_slug):
+        products_list=Product.objects.all().filter(available=True)
+    paginator=Paginator(products_list,6)
     try:
-        product=Product.objects.get(catgory__slug=c_slug,slug=Product_slug)
+        page=int(request.GET.get('page','1'))
+    except:
+        page=1
+    try:
+        products=paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        products=paginator.page(paginator.num_pages)
+
+    return render(request,"category.html",{'category':c_page,'products':products})
+
+def proDetail(request,c_slug,product_slug):
+    try:
+        product=Product.objects.get(Category__slug=c_slug,slug=product_slug)
     except Exception as e:
         raise e
     return render(request,'product.html',{'product':product} )
