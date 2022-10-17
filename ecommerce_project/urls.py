d4b7c56389048c2fa0ae7babd6d5ceaa04d6622d
SARANG S <sarangsvkm@gmail.com>
2022-10-17 14:54:19 +0530

    first

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('shop.urls')),
-    path('search_app/',include('search_app.urls')),
+    path('shop/',include('shop.urls')),
+    path('search/',include('search_app.urls')),
 ]
 
 if settings.DEBUG:
